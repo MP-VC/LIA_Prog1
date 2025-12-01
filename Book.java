f46e2ceb91aeca8efffcd0f13ce696a562b5c424
MP-VC <2519989@edu.vaniercollege.qc.ca>
2025-12-01 15:43:07 -0500

    uncomplete code for book

--- Book.java
+++ Book.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Write a description of class Book here.
@@ -10,7 +12,7 @@ import java.util.Date;
 public class Book
 {
     // instance variables - replace the example below with your own
-    private HashMap<String,Recipe> recipes;
+    private HashMap<String,Recipe> recipe;
     private String author;
     private Date dayPublished;
     /**
@@ -20,4 +22,25 @@ public class Book
     {
        
     }
+    
+    //public void addRecipe(String name)
+    //{
+    //  Recipe r = new Recipe(ingredient,instruction,portion,rating);
+    //  recipe.put(name,r);
+    //}
+    //public void defaultRecipe()
+    //{
+    //    ArrayList<Integer> rat1 = new ArrayList<>();
+    //    int por1 = 1;
+     //   ArrayList<String> ins1 = new ArrayList<>();
+    //    String name ="One day blinding soup";
+    //    int sugar = 1000;
+    //    int protein = 0;
+    //    HashMap<String,Integer> vitamins = new HashMap<>();
+    //    Set<Tag> tag;
+    //    Unit unit;
+    //    Ingredient ingredient = new Ingredient(name, sugar, protein, vitamins,tag,unit);
+    //    HashMap<Ingredient,Double> ing1 = new HashMap<>();
+    //    Recipe r = new Recipe(ing1,ins1,por1,rat1);
+    //}
 }
